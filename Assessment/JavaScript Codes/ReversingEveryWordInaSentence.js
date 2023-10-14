7052ea229bf899a7e4f9b39b2214e4fb0849612a
LingaMahesh11 <lingamaheswarsandu@gmail.com>
2023-10-14 13:37:48 +0530

    First Commit

---
+++ Assessment/JavaScript Codes/ReversingEveryWordInaSentence.js
@@ -0,0 +1,17 @@
+function reverseWordsInSentence(sentence) {
+  
+  const words = sentence.split(' ');
+
+  const reversedWords = words.map(word => {
+    return word.split('').reverse().join('');
+  });
+
+  const reversedSentence = reversedWords.join(' ');
+
+  return reversedSentence;
+}
+
+const inputSentence = "This is a sunny day";
+const reversedSentence = reverseWordsInSentence(inputSentence);
+
+console.log(reversedSentence); // Output: "shiT si a ynnus yad"
